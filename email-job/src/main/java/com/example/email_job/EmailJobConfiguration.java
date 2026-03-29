4363159ed7c4f8811d0b1faeb6e982121c23ab2c
Josh Long <joshua.long@broadcom.com>
2026-03-28 19:16:28 -0700

    native

--- email-job/src/main/java/com/example/email_job/EmailJobConfiguration.java
+++ email-job/src/main/java/com/example/email_job/EmailJobConfiguration.java
@@ -1,17 +1,35 @@
 package com.example.email_job;
 
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Configuration
+@ImportRuntimeHints(EmailJobConfiguration.Hints.class)
 class EmailJobConfiguration {
 
     EmailJobConfiguration() {
         IO.println("registering jobs on startup.");
     }
 
+
+    static class Hints implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(
+                @NonNull RuntimeHints hints,
+                @Nullable ClassLoader classLoader) {
+
+            hints.reflection().registerType(DefaultHelloworldJob.class);
+        }
+    }
+
     @Bean
-    HelloworldJob helloworldJob() {
+    DefaultHelloworldJob helloworldJob() {
         return new DefaultHelloworldJob();
     }
 }
